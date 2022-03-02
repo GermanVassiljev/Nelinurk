53e1db6e0094a58b5f0c83263aba75b1b5beb598
Opilane TTHK <opilane@tthk.ee>
2022-03-02 14:16:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelinurk
+{
+    class Nelinurk
+    {
+        int kõrgus;
+        int laius;
+        string tüüp;
+        public string Tüüp
+        {
+            set
+            {
+                string tüüp;
+                if (kõrgus == laius)
+                {
+                    tüüp = "Ruut";
+                }
+                else if (kõrgus != laius)
+                {
+                    tüüp = "Ristkülik";
+                }
+                else
+                {
+                    tüüp = "Ebakorrapärane ruut";
+                }
+            }
+            get
+            {
+                return tüüp;
+            }
+        }
+        public Nelinurk() { }
+        public Nelinurk(int kõrgus, int laius)
+        {
+
+        }
+        public double Ala
+        {
+            set
+            {
+                double S;
+                if (tüüp=="Ruut")
+                {
+                    S = kõrgus * kõrgus;
+                }
+                else if (tüüp=="Ristkülik")
+                {
+                    S = kõrgus * laius;
+                }
+
+            }
+            get
+            {
+                return S;
+            }
+        }
+        public double Ümbermõõt
+        { }
+    }
+}
